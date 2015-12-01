3a609649aa46c6e2454b5ac9b7eaa7257e529a34
Nonau <arnaud.grima@gmail.com>
2015-12-01 11:03:52 +0100

    push test

--- index.php
+++ index.php
@@ -74,7 +74,7 @@
 
       <!-- Main jumbotron for a primary marketing message or call to action -->
       <div class="jumbotron">
-        <h1>AutoPull BIS Succeeded ! Test terminal</h1>
+        <h1>OK</h1>
         <?php
           $pdo = PDOSingleton::getInstance();
           $users = $pdo->query("SELECT * FROM user")->fetch();
